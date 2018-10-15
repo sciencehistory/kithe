8abf50c1b9dcd30dcd096901a5212e1faf37643e
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-10-15 08:37:22 -0600

    use UUID for kithe models pk

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_03_195235) do
+ActiveRecord::Schema.define(version: 2018_10_15_143413) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "kithe_models", force: :cascade do |t|
+  create_table "kithe_models", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title", null: false
     t.string "type", null: false
     t.integer "position"
     t.jsonb "json_attributes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "parent_id"
+    t.uuid "parent_id"
     t.index ["parent_id"], name: "index_kithe_models_on_parent_id"
   end
 
