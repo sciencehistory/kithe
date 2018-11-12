d7003c1f72874d663995a9cc8237d420e54385ae
Jonathan Rochkind <jrochkind@chemheritage.org>
2018-11-12 16:38:19 -0500

    add original_filename to delegate methods

--- app/models/kithe/asset.rb
+++ app/models/kithe/asset.rb
@@ -4,6 +4,6 @@ class Kithe::Asset < Kithe::Model
   include Kithe::AssetUploader::Attachment.new(:file)
 
   # for convenience, let's delegate some things to shrine parts
-  delegate :content_type, :size, :height, :width, to: :file, allow_nil: true
+  delegate :content_type, :original_filename, :size, :height, :width, to: :file, allow_nil: true
   delegate :stored?, to: :file_attacher
 end
