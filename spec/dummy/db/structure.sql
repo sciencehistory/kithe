3304259166a5df54149b2d0bb98bf6de3ec73ac5
Eddie Rubeiz <erubeiz@sciencehistory.org>
2019-04-05 11:20:45 -0400

    Oops -- travis.yml defined addons twice. The second one was probably overwriting the first.

--- spec/dummy/db/structure.sql
+++ spec/dummy/db/structure.sql
@@ -1,7 +1,5 @@
 SET statement_timeout = 0;
--- START TEST  Commenting this out -- just to test.
--- SET lock_timeout = 0;
--- END TEST
+SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
