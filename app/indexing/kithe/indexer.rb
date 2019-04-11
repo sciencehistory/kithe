9aa95dbb6fe364de0b5342c20e57a1a78c18394c
Jonathan Rochkind <jrochkind@chemheritage.org>
2019-04-11 13:59:49 -0400

    Kithe::Indexer header comment docs

--- app/indexing/kithe/indexer.rb
+++ app/indexing/kithe/indexer.rb
@@ -1,6 +1,26 @@
 require 'traject'
 
 module Kithe
+  # A sub-class of Traject::Indexer, set up for kithe use.
+  #
+  # * It mixes in Kithe::Indexer::ObjExtract for our `obj_extract` macro, useful for
+  #   using traject to index plain old ruby objects as source records
+  #
+  # * it has settings disabling internal threading by setting processing_thread_pool to 0
+  #
+  # * It sets the traject logger to the current Rails.logger, to get your traject obj
+  #   logs going to the right place.
+  #
+  # * It sets `writer_class_name` to something that will raise if you try to use it,
+  #   becuase we don't intend to use an Indexer with a component writer. Kithe's use of
+  #   traject decouples Indexers and Writers.
+  #
+  # * A Kithe::Indexer will automatically index the source record #id to Solr object
+  #   #id, and the source record class name to Solr field `model_name_ssi`. (That uses
+  #   Blacklight conventions for dynamic field names, if you'd like to change the field name
+  #   used, set `Kithe::Indexable.settings.model_name_solr_field=`)
+  #
+  #   ID and model_name are set, so the AR object can be easily fetched later from Solr results.
   class Indexer < Traject::Indexer
     include Kithe::Indexer::ObjExtract
 
