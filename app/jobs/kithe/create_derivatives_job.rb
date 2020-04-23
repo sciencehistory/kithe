6c34559060342e6c2708351dbbab6bc7b5f9667e
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-04-23 11:46:22 -0400

    rescue from ActiveJob::DeserializationError during deriv creation.

--- app/jobs/kithe/create_derivatives_job.rb
+++ app/jobs/kithe/create_derivatives_job.rb
@@ -1,7 +1,12 @@
 module Kithe
   class CreateDerivativesJob < Job
     def perform(asset, mark_created: nil)
-      asset.create_derivatives(mark_created: mark_created)
+      begin
+        asset.create_derivatives(mark_created: mark_created)
+      rescue ActiveJob::DeserializationError
+        Rails.logger.error("Unable to derivatives for asset with ID #{asset.id}, as it was deleted first.")
+        return
+      end
     end
   end
 end
