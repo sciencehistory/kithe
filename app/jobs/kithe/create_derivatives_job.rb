36a48489a3bacd20c7a9334809e26e8bca06e15c
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-04-27 17:13:55 -0400

    Added test and using ActiveJob's rescue_from method

--- app/jobs/kithe/create_derivatives_job.rb
+++ app/jobs/kithe/create_derivatives_job.rb
@@ -1,12 +1,13 @@
 module Kithe
   class CreateDerivativesJob < Job
     def perform(asset, mark_created: nil)
-      begin
         asset.create_derivatives(mark_created: mark_created)
-      rescue ActiveJob::DeserializationError
-        Rails.logger.error("Unable to derivatives for asset with ID #{asset.id}, as it was deleted first.")
-        return
-      end
+    end
+    # This error typically occurs when several large assets, whose derivatives
+    # take a long time to generate, are deleted immediately after ingest.
+    rescue_from(ActiveJob::DeserializationError) do |exception|
+      Rails.logger.error("Unable to derivatives for this asset, as it was unavailable.")
+      Rails.logger.error("Details: #{exception.inspect}")
     end
   end
 end
