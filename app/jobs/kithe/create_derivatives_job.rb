67687ae183613616835b4fb9d38c5ee2aec3feac
Eddie Rubeiz <erubeiz@sciencehistory.org>
2020-04-28 11:51:01 -0400

    adding class name to error message

--- app/jobs/kithe/create_derivatives_job.rb
+++ app/jobs/kithe/create_derivatives_job.rb
@@ -6,7 +6,7 @@ module Kithe
     # This error typically occurs when several large assets, whose derivatives
     # take a long time to generate, are deleted immediately after ingest.
     rescue_from(ActiveJob::DeserializationError) do |exception|
-      Rails.logger.error("Unable to create derivatives for this asset, as it was unavailable. Details: #{exception}")
+      Rails.logger.error("Kithe::CreateDerivativesJob: Unable to create derivatives for this asset, as it was unavailable. Details: #{exception}")
     end
   end
 end
