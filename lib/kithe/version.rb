83581b09405b8440db7ab0ff8361252627ffe3ef
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-05-26 10:23:56 -0400

    call it .pre.beta1 to make rubygems releases consistent

--- lib/kithe/version.rb
+++ lib/kithe/version.rb
@@ -1,3 +1,6 @@
 module Kithe
-  VERSION = '2.0.0.beta1'
+  # not sure why rubygems turned our alphas into 2.0.0.pre.alpha1, inserting
+  # "pre". We need to do same thing with betas to get version orderings
+  # appropriate.
+  VERSION = '2.0.0.pre.beta1'
 end
