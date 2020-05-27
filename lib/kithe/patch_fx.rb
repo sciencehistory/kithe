d1402c0d2345092e085dcfbc2ad02488b224ac90
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-05-27 12:20:17 -0400

    patch to fx gem attempts forward-safety, 'polyfill' style
    https://github.com/teoljungberg/fx/pull/53

--- lib/kithe/patch_fx.rb
+++ lib/kithe/patch_fx.rb
@@ -8,17 +8,32 @@
 # in a patch to force it. Better than forking.
 #
 # Based on: https://github.com/teoljungberg/fx/pull/53/
+#
+# We try to write future-compat code assuming that will be merged eventually....
 
-require 'fx/schema_dumper/function'
+require 'fx'
 
-module Fx
-  module SchemaDumper
-    module Function
-      def tables(stream)
-        functions(stream)
-        super
+if Fx.configuration.respond_to?(:dump_functions_at_beginning_of_schema)
+  # we have the feature!
+
+  Fx.configure do |config|
+    config.dump_functions_at_beginning_of_schema = true
+  end
+
+else
+  # Fx does not have the feature, we have to patch it in
+
+  require 'fx/schema_dumper/function'
+
+  module Fx
+    module SchemaDumper
+      module Function
+        def tables(stream)
+          functions(stream)
+          super
+        end
       end
     end
   end
-end
 
+end
