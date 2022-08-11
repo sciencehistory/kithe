23b5b05a157e747daed4c02ac7be4d11a9a3601c
Jonathan Rochkind <jonathan@dnil.net>
2022-08-11 11:20:01 -0400

    Allow Kithe.indexable_settings.writer_settings to be mutated as per docs
    The way we were creating a new hash dynamically on each method call means mutating the return value was not preserved. This broke existing examples in kithe docs like `Kithe.indexable_settings.writer_settings.merge!`.
    
    We fix that, while still preserving the fact that changing #solr_url has an effect on #writer_settings, by simply giving it that side effect.

--- spec/models/kithe_spec.rb
+++ spec/models/kithe_spec.rb
@@ -6,5 +6,19 @@ describe "Kithe module" do
       expect(Rails.logger).not_to be_nil
       expect(Kithe.indexable_settings.writer_settings["logger"]).to be(Rails.logger)
     end
+
+    describe "#writer_settings" do
+      it "allows mutation by key" do
+        Kithe.indexable_settings.writer_settings["solr_writer.http_timeout"] = 10
+        expect(Kithe.indexable_settings.writer_settings["solr_writer.http_timeout"]).to eq(10)
+      end
+
+      it "allows mutation by merge!" do
+        Kithe.indexable_settings.writer_settings.merge!(
+          "solr_writer.solr_update_args" => {}
+        )
+        expect(Kithe.indexable_settings.writer_settings["solr_writer.solr_update_args"]).to eq({})
+      end
+    end
   end
 end
